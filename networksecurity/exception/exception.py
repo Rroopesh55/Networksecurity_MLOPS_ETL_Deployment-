434e1546193bf1a3b19e8a3824298e1c75db8d61
Rroopesh Hari <rroopesh.hari@okstate.edu>
2025-08-30 23:33:40 -0500

    MongoDB Integration

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -8,10 +8,4 @@ class NetworkSecurityException(Exception):
         self.file_name = exc_tb.tb_frame.f_code.co_filename
         self.line_number = exc_tb.tb_lineno
         
-    def __str__(self):
-        return f"Error Occured in script name [{0}] line number [{1}] error message [{2}]".format(
-            self.file_name,
-            self.line_number,
-            str(self.error_message)
-        )
         
